5edfcbc83a49c089a1426ea58c041189a473246d
odyssic <stevenbuddenjr@gmail.com>
2021-04-09 13:20:21 -0400

    controls built in

--- main.js
+++ main.js
@@ -1,59 +1,79 @@
-window.onload = init
-
-
+window.onload = init;
 
 function init() {
+    // Controls
+    const fullScreenControl = new ol.control.FullScreen();
+    const mousePositionControl = new ol.control.MousePosition();
+    const overViewMapControl = new ol.control.OverviewMap({
+        collapsed: false,
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ]
+    });
+    const scaleLineControl = new ol.control.ScaleLine();
+    const zoomSliderControl = new ol.control.ZoomSlider();
+    const zoomToExtentControl = new ol.control.ZoomToExtent();
+
     const map = new ol.Map({
         view: new ol.View({
-            // x and y coords
             center: [-12080385, 7567433],
-
-            zoom: 2,
+            zoom: 3,
             maxZoom: 6,
             minZoom: 2,
-            rotation: 0,
-
-
+            rotation: 0
         }),
         layers: [
             new ol.layer.Tile({
                 source: new ol.source.OSM()
             })
         ],
-        target: "js-map",
-        keyboardEventTarget: document
+        target: 'js-map',
+        keyboardEventTarget: document,
+        controls: ol.control.defaults().extend([
+            fullScreenControl,
+            mousePositionControl,
+            overViewMapControl,
+            scaleLineControl,
+            zoomSliderControl,
+            zoomToExtentControl
+        ])
     })
 
-    const popupContainerELement = document.getElementById('popup-coordinates');
 
+    const popupContainerElement = document.getElementById('popup-coordinates');
     const popup = new ol.Overlay({
-        element: popupContainerELement,
-        positioning: 'center-center',
+        element: popupContainerElement,
+        positioning: 'top-right'
     })
 
     map.addOverlay(popup);
 
-    map.on('click', (e) => {
-            console.log(e.coordinate)
-            const clickedCoordinate = e.coordinate;
-            // popup.setPosition(undefined)
-            popup.setPosition(clickedCoordinate)
-            popupContainerELement.innerHTML = clickedCoordinate;
+    map.on('click', function(e) {
+        const clickedCoordinate = e.coordinate;
+        popup.setPosition(undefined);
+        popup.setPosition(clickedCoordinate);
+        popupContainerElement.innerHTML = clickedCoordinate;
+    })
 
-
-        })
-        //  DragRotate
+    // DragRotate Interaction
     const dragRotateInteraction = new ol.interaction.DragRotate({
         condition: ol.events.condition.altKeyOnly
     })
 
     map.addInteraction(dragRotateInteraction)
 
+    // Draw Interaction
     const drawInteraction = new ol.interaction.Draw({
-        type: 'Polygon'
+        type: 'Polygon',
+        freehand: true
     })
+    map.addInteraction(drawInteraction);
 
-    map.addInteraction(drawInteraction)
-
-
+    drawInteraction.on('drawend', function(e) {
+        let parser = new ol.format.GeoJSON();
+        let drawnFeatures = parser.writeFeatures([e.feature]);
+        console.log(drawnFeatures);
+    })
 }
